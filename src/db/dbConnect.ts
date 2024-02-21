8fd7b59e183d35e7eab60e9fe89d269bdbfa4580
Liudmy-la <lyouda.malko@gmail.com>
2024-02-21 20:41:18 +0200

    test connection with local db

--- src/db/dbConnect.ts
+++ src/db/dbConnect.ts
@@ -23,3 +23,16 @@ export async function connect() {
 }
 
 export default connect
+
+// Client connection 
+// import { drizzle } from "drizzle-orm/mysql2";
+// import mysql from "mysql2/promise";
+
+// const connection = await mysql.createConnection({
+//   host: "host",
+//   user: "user",
+//   database: "database",
+//   ...
+// });
+
+// const db = drizzle(connection);
